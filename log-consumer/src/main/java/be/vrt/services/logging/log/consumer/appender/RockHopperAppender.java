f08e77a90a8fb78694ab175ba167d9244d163638
luc steffens <luc.steffens@vrt.be>
2015-04-17 13:32:27 +0200

    SVC-3170 Rock Hopping

--- log-consumer/src/main/java/be/vrt/services/logging/log/consumer/appender/RockHopperAppender.java
+++ log-consumer/src/main/java/be/vrt/services/logging/log/consumer/appender/RockHopperAppender.java
@@ -1,5 +1,46 @@
 package be.vrt.services.logging.log.consumer.appender;
 
-public class RockHopperAppender {
+import java.net.DatagramPacket;
+import java.net.DatagramSocket;
+import java.net.InetAddress;
+import java.net.SocketException;
+
+public class RockHopperAppender extends AbstractJsonAppender {
+
+	private int port = 5514;
+	private DatagramSocket clientSocket;
+
+	@Override
+	public void start() {
+		super.start();
+		try {
+			clientSocket = new DatagramSocket(port, InetAddress.getByName("localhost"));
+		} catch (Exception e) {
+			System.err.println("Failed to connect to UDP on localhost:" + port);
+		}
+	}
+
+	@Override
+	protected void persist(String json) {
+		try {
+			if (!clientSocket.isConnected()) {
+				try {
+					clientSocket = new DatagramSocket(port);
+				} catch (Exception e) {
+					System.err.println("Failed to connect to UDP on localhost:" + port);
+				}
+			}
+			byte[] sendData = json.getBytes();
+			DatagramPacket sendPacket = new DatagramPacket(sendData, sendData.length);
+			clientSocket.send(sendPacket);
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+	}
+
+	@Override
+	public void stop() {
+		clientSocket.close();
+	}
 
 }
