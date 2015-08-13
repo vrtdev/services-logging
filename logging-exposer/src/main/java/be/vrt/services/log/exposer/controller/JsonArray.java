934f79772b70559c8fac9de6b3597556124dd97a
Kobe Van de Sande <kobe.vandesande@vrt.be>
2015-08-13 09:55:59 +0200

    Fixed some warnings + removed junit version from child pom

--- logging-exposer/src/main/java/be/vrt/services/log/exposer/controller/JsonArray.java
+++ logging-exposer/src/main/java/be/vrt/services/log/exposer/controller/JsonArray.java
@@ -2,15 +2,15 @@ package be.vrt.services.log.exposer.controller;
 
 import java.util.LinkedList;
 
-public class JsonArray extends LinkedList<Object>{
-
-
-    public static JsonArray with(Object... args) {
-        JsonArray jsonArray = new JsonArray();
-        for (Object arg : args) {
-            jsonArray.add(arg);
-        }
-        return jsonArray;
-
-    }
+@SuppressWarnings("serial")
+public class JsonArray extends LinkedList<Object> {
+	
+	public static JsonArray with(Object... args) {
+		JsonArray jsonArray = new JsonArray();
+		for (Object arg : args) {
+			jsonArray.add(arg);
+		}
+		return jsonArray;
+		
+	}
 }
