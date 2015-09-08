1a68be3d9d1898dc3df8830471123fcd29b127aa
Luc Steffens <luc.steffens@vrt.be>
2015-09-08 08:39:34 +0200

    SVC-3439 1 JS loading async issue

--- logging-exposer/src/main/resources/META-INF/resources/services/logging/log-init.js
+++ logging-exposer/src/main/resources/META-INF/resources/services/logging/log-init.js
@@ -2,6 +2,17 @@ if (!document.rootContextUrl) {
 	document.rootContextUrl = "";
 }
 
+var loadFiles = [
+	"services/logging/lib/momentjs.js",
+	"services/logging/lib/highcharts.js",
+	"services/logging/lib/highcharts-config.js",
+	"services/logging/logging.js?v=${mvn.timestamp}",
+	"services/logging/log-stats.js?v=${mvn.timestamp}"
+]
+var counter = 0;
+
+document.afterLoad = [];
+
 function loadjscssfile(filename, filetype) {
 	var context = filename.search(/\/\//) !== -1 ? "" : document.rootContextUrl;
 	if (filetype == "js") { //if filename is a external JavaScript file
@@ -18,16 +29,23 @@ function loadjscssfile(filename, filetype) {
 	if (typeof fileref != "undefined")
 		document.getElementsByTagName("head")[0].appendChild(fileref);
 }
+loadjscssfile("services/logging/logging.css?v=${mvn.timestamp}", "css");
+loadjscssfile("https://fonts.googleapis.com/css?family=Ubuntu+Mono", "css");
 
 if (typeof jQuery === 'undefined') {
 	loadjscssfile("https://ajax.googleapis.com/ajax/libs/jquery/2.1.3/jquery.min.js", "js");
+	setTimeout(function (){loadScript(0)}, 1000)
+} else {
+	loadScript(0);
 }
-loadjscssfile("services/logging/logging.css?v=${mvn.timestamp}", "css");
-loadjscssfile("https://fonts.googleapis.com/css?family=Ubuntu+Mono", "css");
-loadjscssfile("services/logging/lib/momentjs.js", "js");
-loadjscssfile("services/logging/lib/highcharts.js", "js");
-loadjscssfile("services/logging/lib/highcharts-config.js", "js");
 
-
-loadjscssfile("services/logging/logging.js?v=${mvn.timestamp}", "js");
-loadjscssfile("services/logging/log-stats.js?v=${mvn.timestamp}", "js");
+function loadScript(i){
+	if (i>= loadFiles.length){
+		document.afterLoad.forEach(function (func){
+			func();
+		});
+		return
+	}
+	var context = loadFiles[i].search(/\/\//) !== -1 ? "" : document.rootContextUrl;
+	$.getScript(context + loadFiles[i], function (){loadScript(++i)});
+}
