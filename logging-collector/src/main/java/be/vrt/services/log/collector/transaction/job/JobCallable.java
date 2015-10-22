5e854c890c843c8b39d987c5ce9e97588fe8bdcd
Marc Isenborghs <marc.isenborghs@vrt.be>
2015-10-22 17:54:09 +0200

    SVC-3650: java 7

--- logging-collector/src/main/java/be/vrt/services/log/collector/transaction/job/JobCallable.java
+++ logging-collector/src/main/java/be/vrt/services/log/collector/transaction/job/JobCallable.java
@@ -2,7 +2,7 @@ package be.vrt.services.log.collector.transaction.job;
 
 import be.vrt.services.log.collector.exception.FailureException;
 
-@FunctionalInterface
+
 public interface JobCallable<V> {
 
 	V call() throws FailureException;
