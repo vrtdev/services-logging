0858078a6dbf08443765bca39bb06b0b577d1126
Tim Boeckstaens <boeckstaenstim@gmail.com>
2017-07-14 12:20:23 +0200

    SVC-5275: replace concatenation with parametrized string

--- logging-collector/src/main/java/be/vrt/services/log/collector/audit/aspect/BasicBreadcrumbAuditAspect.java
+++ logging-collector/src/main/java/be/vrt/services/log/collector/audit/aspect/BasicBreadcrumbAuditAspect.java
@@ -11,19 +11,19 @@ import java.util.function.Consumer;
 public class BasicBreadcrumbAuditAspect extends AbstractBreadcrumbAuditAspect {
     @Override
     protected Object handleJoinPoint(ProceedingJoinPoint joinPoint) throws Throwable {
-        log(joinPoint, l -> l.debug("[BREADCRUMB] " + joinPoint.getSignature().toShortString()));
+        log(joinPoint, "[BREADCRUMB] {}", joinPoint.getSignature().toShortString());
         try {
             return joinPoint.proceed();
         } catch (Exception e) {
-            log(joinPoint, l -> l.debug(
-                    "[BREADCRUMB-EXCEPTION] " + joinPoint.getSignature().toShortString() + " -> " + e.getMessage(), e));
+            log(joinPoint, "[BREADCRUMB-EXCEPTION] {} -> {}",
+                    joinPoint.getSignature().toShortString(), e.getMessage(), e);
             throw e;
         }
     }
 
-    private void log(ProceedingJoinPoint jointPoint, Consumer<Logger> consumer) {
+    private void log(ProceedingJoinPoint jointPoint, String template, Object... args) {
         if (!Level.OFF.name().equals(LogTransaction.getLevel())) {
-            consumer.accept(LoggerFactory.getLogger(jointPoint.getTarget().getClass()));
+            LoggerFactory.getLogger(jointPoint.getTarget().getClass()).debug(template, args);
         }
     }
 }
