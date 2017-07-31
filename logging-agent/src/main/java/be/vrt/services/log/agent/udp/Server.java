61cb3c806c76cb2dd160a7087f942cb11576e6eb
Kobe Van de Sande <kobe.vandesande@vrt.be>
2017-07-31 14:49:18 +0200

    SVC-5299 Increase receive buffer size to 4096 bytes (instead of 2048 bytes)

--- logging-agent/src/main/java/be/vrt/services/log/agent/udp/Server.java
+++ logging-agent/src/main/java/be/vrt/services/log/agent/udp/Server.java
@@ -3,6 +3,7 @@ package be.vrt.services.log.agent.udp;
 import be.vrt.services.log.agent.log.LogBuilder;
 import io.netty.bootstrap.Bootstrap;
 import io.netty.channel.ChannelHandlerContext;
+import io.netty.channel.ChannelOption;
 import io.netty.channel.SimpleChannelInboundHandler;
 import io.netty.channel.nio.NioEventLoopGroup;
 import io.netty.channel.socket.DatagramPacket;
@@ -15,6 +16,7 @@ import org.slf4j.LoggerFactory;
 public class Server {
 
     private static final Logger LOGGER = LoggerFactory.getLogger(Server.class);
+    private static final int RCVBUF_BYTES = 4096;
 
     private DefaultThreadFactory acceptFactory = new DefaultThreadFactory("accept");
     private NioEventLoopGroup acceptGroup = new NioEventLoopGroup(1, acceptFactory);
@@ -31,6 +33,7 @@ public class Server {
         new Bootstrap()
                 .group(acceptGroup)
                 .channel(NioDatagramChannel.class)
+                .option(ChannelOption.SO_RCVBUF, RCVBUF_BYTES)
                 .handler(new Handler())
                 .bind(port).sync();
     }
