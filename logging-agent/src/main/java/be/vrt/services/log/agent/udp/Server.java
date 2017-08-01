d7e7fb0f383a6f883bd33ebade0eef108d587b8b
Kobe Van de Sande <kobe.vandesande@vrt.be>
2017-08-01 13:05:09 +0200

    SVC-5299 Increase receive buffer size to 64k (instead of 2048 bytes) (4k too low, 8k bare minimum) + updated netty to v 4.1.13.Final

--- logging-agent/src/main/java/be/vrt/services/log/agent/udp/Server.java
+++ logging-agent/src/main/java/be/vrt/services/log/agent/udp/Server.java
@@ -2,9 +2,7 @@ package be.vrt.services.log.agent.udp;
 
 import be.vrt.services.log.agent.log.LogBuilder;
 import io.netty.bootstrap.Bootstrap;
-import io.netty.channel.ChannelHandlerContext;
-import io.netty.channel.ChannelOption;
-import io.netty.channel.SimpleChannelInboundHandler;
+import io.netty.channel.*;
 import io.netty.channel.nio.NioEventLoopGroup;
 import io.netty.channel.socket.DatagramPacket;
 import io.netty.channel.socket.nio.NioDatagramChannel;
@@ -16,7 +14,7 @@ import org.slf4j.LoggerFactory;
 public class Server {
 
     private static final Logger LOGGER = LoggerFactory.getLogger(Server.class);
-    private static final int RCVBUF_BYTES = 4096;
+    private static final int RCVBUF_BYTES = 65536;
 
     private DefaultThreadFactory acceptFactory = new DefaultThreadFactory("accept");
     private NioEventLoopGroup acceptGroup = new NioEventLoopGroup(1, acceptFactory);
@@ -34,6 +32,7 @@ public class Server {
                 .group(acceptGroup)
                 .channel(NioDatagramChannel.class)
                 .option(ChannelOption.SO_RCVBUF, RCVBUF_BYTES)
+                .option(ChannelOption.RCVBUF_ALLOCATOR, new FixedRecvByteBufAllocator(RCVBUF_BYTES))
                 .handler(new Handler())
                 .bind(port).sync();
     }
